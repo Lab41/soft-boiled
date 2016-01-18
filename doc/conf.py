fd5596a85315e838be493f087c7e06e9cf1d0814
Yonas Tesfaye <ymt123@alum.mit.edu>
2016-01-17 16:59:57 -0800

    Attempting to update mock to correctly mock statsmodel

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,7 @@ if on_rtd:
         def __getattr__(cls, name):
             return Mock()
     MOCK_MODULES = ['pyspark.RDD', 'numpy', 'pandas', 'matplotlib.pyplot','sklearn',
-                 'pyspark', 'statsmodels.sandbox.distributions.extras','statsmodels', 'matplotlib']
+                 'pyspark', 'statsmodels.sandbox.distributions','statsmodels', 'matplotlib']
     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
